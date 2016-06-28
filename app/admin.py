cea9a06ebee9230f3e19ef84d0c9da7bbf7264c0
williamjohngardner <bill@williamjohngardner.com>
2016-06-28 13:35:46 -0400

    Data Migrations Complete

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Movie, Rater, Rating
 
-# Register your models here.
+
+admin.site.register(Movie),
+admin.site.register(Rater),
+admin.site.register(Rating)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=60)
+    release_date = models.CharField(max_length=15)
+    video_release_date = models.CharField(max_length=10, default="")
+    IMDb_URL = models.URLField()
+    unknown = models.IntegerField()
+    action = models.IntegerField()
+    adventure = models.IntegerField()
+    animation = models.IntegerField()
+    childrens = models.IntegerField()
+    comedy = models.IntegerField()
+    crime = models.IntegerField()
+    documentary = models.IntegerField()
+    drama = models.IntegerField()
+    fantasy = models.IntegerField()
+    film_noir = models.IntegerField()
+    horror = models.IntegerField()
+    musical = models.IntegerField()
+    mystery = models.IntegerField()
+    romance = models.IntegerField()
+    sci_fi = models.IntegerField()
+    thriller = models.IntegerField()
+    war = models.IntegerField()
+    western = models.IntegerField()
+
+    def __str__(self):
+        return self.movie_title
+
+
+class Rater(models.Model):
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    occupation = models.CharField(max_length=30)
+    zip_code = models.CharField(max_length=6)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(Rater)
+    item = models.ForeignKey(Movie)
+    rating = models.IntegerField()
+    timestamp = models.IntegerField()
+
+    def __str__(self):
+        return str(self.rating)
